3bd7299889cb45dc03af935bd4734c4bf62af390
NikBel3476 <nikita.belkin3476@gmail.com>
2022-03-15 00:04:32 +0400

    feature: add wind data list
    it needs to replace to independent component

--- src/components/DateForm/DateForm.tsx
+++ src/components/DateForm/DateForm.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DateInput from "../DateInput";
-
 import styles from "./DateForm.module.css";
 import { getWindStats } from "../../http/weatherAPI";
 import { $city } from "../../http";
@@ -10,10 +9,13 @@ const DateForm = () => {
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [endDate, setEndDate] = useState<Date>(new Date());
   const [cities, setCities] = useState<City[]>([]);
+  const [days, setDays] = useState<any>([]); // TODO: delete any type
 
   useEffect(() => {
     loadCitiesData();
   }, []);
+
+  const citySelect = useRef<HTMLSelectElement>(null);
 
   const loadCitiesData = async () => {
     const res = await $city.get<City[]>("/");
@@ -22,50 +24,67 @@ const DateForm = () => {
 
   const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setStartDate(new Date(e.target.value));
-    console.log(startDate);
   };
 
   const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEndDate(new Date(e.target.value));
-    console.log(endDate);
   };
 
   const onSubmit = async (e: React.MouseEvent) => {
     e.preventDefault();
-    const res = await getWindStats(startDate, endDate);
-    console.log(res.data);
+    if (citySelect.current) {
+      const res = await getWindStats(
+        cities[citySelect.current.selectedIndex].name,
+        startDate,
+        endDate
+      );
+      setDays(res.data.days);
+    } else {
+      throw new Error("Error with city name");
+    }
   };
 
+  // TODO: leave only the form
   return (
-    <form className={styles.form}>
-      <h1>Информация о ветре</h1>
-      <div>
-        <DateInput
-          className={styles.date}
-          value={new Date()}
-          onChange={handleStartDateChange}
-          max={endDate}
-        />
-        <DateInput
-          className={styles.date}
-          value={new Date()}
-          onChange={handleEndDateChange}
-          min={startDate}
-        />
-      </div>
-      <select className={styles.citiesList}>
-        {cities.map((city) => (
-          <option key={city.id}>{city.name}</option>
+    <div>
+      <form className={styles.form}>
+        <h1>Информация о ветре</h1>
+        <div>
+          <DateInput
+            className={styles.date}
+            value={new Date()}
+            onChange={handleStartDateChange}
+            max={endDate}
+          />
+          <DateInput
+            className={styles.date}
+            value={new Date()}
+            onChange={handleEndDateChange}
+            min={startDate}
+          />
+        </div>
+        <select ref={citySelect} className={styles.citiesList}>
+          {cities.map((city) => (
+            <option key={city.id}>{city.name}</option>
+          ))}
+        </select>
+        <button
+          className={styles.submit}
+          type={"submit"}
+          onClick={(e) => onSubmit(e)}
+        >
+          Подтвердить
+        </button>
+      </form>
+      <ul>
+        {days.map((day: any) => (
+          <li>
+            Дата: {day.datetime} Направление: {day.winddir} Скорость:{" "}
+            {day.windspeed} Порывы: {day.windgust}
+          </li>
         ))}
-      </select>
-      <button
-        className={styles.submit}
-        type={"submit"}
-        onClick={(e) => onSubmit(e)}
-      >
-        Подтвердить
-      </button>
-    </form>
+      </ul>
+    </div>
   );
 };
 
--- src/http/weatherAPI.ts
+++ src/http/weatherAPI.ts
@@ -1,9 +1,9 @@
 import { $weather } from "./index";
 
-const getWindStats = async (start: Date, end: Date) => {
+const getWindStats = async (city: string, start: Date, end: Date) => {
   const startISO = start.toISOString().split("T")[0];
   const endISO = end.toISOString().split("T")[0];
-  const data = await $weather.get(`Izhevsk/${startISO}/${endISO}`); // TODO: delete Izhevsk from URL
+  const data = await $weather.get(`${city}/${startISO}/${endISO}`);
   return data;
 };
 
